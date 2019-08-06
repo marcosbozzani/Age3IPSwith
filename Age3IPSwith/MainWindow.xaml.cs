40f80c912eb54136cf18a0c76fad069d8d64e6a8
marcosbozzani <marcosbozzani@gmail.com>
2019-08-06 19:05:02 -0300

    Add project files.

---
+++ Age3IPSwith/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Age3IPSwith
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Info.Text = "Saving...";
+            await Task.Delay(200);
+            SaveData();
+        }
+
+        private async void ReloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Info.Text = "Loading...";
+            await Task.Delay(200);
+            LoadData();
+        }
+
+        private void SaveData()
+        {
+            if (IpCombobox.Items.Count > 0)
+            {
+                Utils.WriteAddress(IpCombobox.SelectedValue.ToString());
+                Info.Text = "Saved";
+            }
+        }
+
+        private void LoadData()
+        {
+            var saved = Utils.ReadAddress();
+
+            IpCombobox.Items.Clear();
+
+            foreach (var ip in Utils.GetMachineIPs())
+            {
+                IpCombobox.Items.Add(ip);
+                if (ip == saved)
+                {
+                    IpCombobox.SelectedValue = ip;
+                }
+            }
+
+            if (IpCombobox.Items.Count > 0 && IpCombobox.SelectedIndex == -1)
+            {
+                IpCombobox.SelectedIndex = 0;
+            }
+
+            Info.Text = "Loaded";
+        }
+    }
+}
---
+++ Age3IPSwith/Utils.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age3IPSwith
+{
+    public class Utils
+    {
+        private static readonly string UserCfgPath;
+        
+        static Utils()
+        {
+            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            UserCfgPath = Path.Combine(documents + @"\My Games\Age of Empires 3\Startup\user.cfg");
+        }
+
+        public static string FindLauncherPath()
+        {
+            var keyPrefixs = new[]
+            {
+                @"SOFTWARE\Wow6432Node\Microsoft\Microsoft Games\",
+                @"SOFTWARE\Microsoft\Microsoft Games\"
+            };
+
+            var gameVersions = new[]
+            {
+                @"Age of Empires 3 Expansion Pack 2\1.0",
+                @"Age of Empires 3 Expansion Pack 1\1.0",
+                @"Age of Empires 3\1.0"
+            };
+
+            var binNames = new[]
+            {
+                "age3y.exe",
+                "age3x.exe",
+                "age3.exe"
+            };
+
+            foreach (var keyPrefix in keyPrefixs)
+            {
+                foreach (var gameVersion in gameVersions)
+                {
+                    var key = Registry.LocalMachine.OpenSubKey(keyPrefix + gameVersion);
+                    if (key != null)
+                    {
+                        var value = key.GetValue("SetupPath");
+                        if (value != null)
+                        {
+                            foreach (var binName in binNames)
+                            {
+                                var path = Path.Combine(value.ToString(), binName);
+                                if (File.Exists(path))
+                                {
+                                    return path;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return "";
+        }
+
+        public static string ReadAddress()
+        {
+            if (File.Exists(UserCfgPath))
+            {
+                using (var reader = new StreamReader(UserCfgPath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        var data = line.Trim().Split('=');
+                        if (data.Length == 2 && data[0].Trim() == "OverrideAddress")
+                        {
+                            return data[1].Trim().Replace("\"", "");
+                        }
+                    }
+                }
+            }
+
+            return "";
+        }
+
+        public static void WriteAddress(string address)
+        {
+            if (File.Exists(UserCfgPath))
+            {
+                var count = 0;
+                var lines = File.ReadAllLines(UserCfgPath);
+
+                foreach (var line in lines)
+                {
+                    var data = line.Trim().Split('=');
+                    if (data.Length == 2 && data[0].Trim() == "OverrideAddress")
+                    {
+                        lines[count] = string.Format("OverrideAddress=\"{0}\"", address);
+                    }
+
+                    count++;
+                }
+
+                File.WriteAllLines(UserCfgPath, lines);
+            }
+            else
+            {
+                var content = string.Format("OverrideAddress=\"{0}\"", address);
+                File.WriteAllText(UserCfgPath, content);
+            }
+        }
+
+        public static IEnumerable<string> GetMachineIPs()
+        {
+            foreach (var netInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                IPInterfaceProperties ipProps = netInterface.GetIPProperties();
+                foreach (var addressInfo in ipProps.UnicastAddresses)
+                {
+                    if (!IPAddress.IsLoopback(addressInfo.Address)
+                        && addressInfo.Address.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        yield return addressInfo.Address.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
